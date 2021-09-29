65f50a13f97c852b3a960cfad633fb85be335927
navistar792 <r.verner99@yahoo.com>
2021-09-29 07:09:27 -0500

    save

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,29 @@
-// from data.js
-var tableData = data;
+// Assign the data from `data.js` to a descriptive variable
+var sightings = data;
 
-// YOUR CODE HERE!
+//select button
+var button = d3.select("#filter-btn");
+
+//select input box
+var box = d3.select(".form-control");
+
+// Create event handlers 
+button.on("click", runEnter);
+box.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    console.log("Button, input and event handlers working ok so far")
+      // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(sightings);
+};
